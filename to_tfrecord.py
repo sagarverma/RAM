9936b50a90eedc43c3da80803a0d524b10539f72
sagarverma <sagar15056@iiitd.ac.in>
2017-10-11 23:43:51 +0530

    to tfrecord

--- to_tfrecord.py
+++ to_tfrecord.py
@@ -58,7 +58,8 @@ def load_image(addr):
     # read an image and resize to (224, 224)
     # cv2 load images as BGR, convert it to RGB
     img = cv2.imread(addr)
-    img = cv2.resize(img, (224, 224), interpolation=cv2.INTER_CUBIC)
+    print img.shape
+    #img = cv2.resize(img, (224, 224), interpolation=cv2.INTER_CUBIC)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img = img.astype(np.float32)
     return img
