468c0a38a887d8947aca469751a68c8cd7453a94
sagarverma <sagar15056@iiitd.ac.in>
2017-10-12 08:44:28 +0530

    read tfrecords

--- read_data.py
+++ read_data.py
@@ -1,150 +1,51 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+data_path = 'train.tfrecords'  # address to save the hdf5 file
 
-import numpy
-from six.moves import xrange  # pylint: disable=redefined-builtin
+inverse_class_map= {0:'bags',1:'belts',2:'dresses',3:'eyewear',4:'footwear',
+    5:'hats',6:'leggings',7:'outerwear',8:'pants',9:'skirts',10:'tops'}
 
-from tensorflow.contrib.learn.python.learn.datasets import base
-from tensorflow.python.framework import dtypes
-from tensorflow.python.framework import random_seed
-from tensorflow.python.platform import gfile
+with tf.Session() as sess:
+    feature = {'train/image': tf.FixedLenFeature([], tf.string),
+               'train/label': tf.FixedLenFeature([], tf.int64)}
+    # Create a list of filenames and pass it to a queue
+    filename_queue = tf.train.string_input_producer([data_path], num_epochs=1)
+    # Define a reader and read the next record
+    reader = tf.TFRecordReader()
+    _, serialized_example = reader.read(filename_queue)
+    # Decode the record read by the reader
+    features = tf.parse_single_example(serialized_example, features=feature)
+    # Convert the image data from string back to the numbers
+    image = tf.decode_raw(features['train/image'], tf.float32)
 
+    # Cast label data into int32
+    label = tf.cast(features['train/label'], tf.int32)
+    # Reshape image data into the original shape
+    image = tf.reshape(image, [256, 256, 3])
 
+    # Any preprocessing here ...
 
-def _read32(bytestream):
-  dt = numpy.dtype(numpy.uint32).newbyteorder('>')
-  return numpy.frombuffer(bytestream.read(4), dtype=dt)[0]
+    # Creates batches by randomly shuffling tensors
+    images, labels = tf.train.shuffle_batch([image, label], batch_size=10, capacity=30, num_threads=1, min_after_dequeue=10)
 
+    # Initialize all global and local variables
+    init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
+    sess.run(init_op)
+    # Create a coordinator and run all QueueRunner objects
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(coord=coord)
+    for batch_index in range(5):
+        img, lbl = sess.run([images, labels])
+        img = img.astype(np.uint8)
+        for j in range(6):
+            plt.subplot(2, 3, j + 1)
+            plt.imshow(img[j, ...])
+            plt.title(inverse_class_map[lbl[j]])
+        plt.show()
+    # Stop the threads
+    coord.request_stop()
 
-def dense_to_one_hot(labels_dense, num_classes):
-  """Convert class labels from scalars to one-hot vectors."""
-  num_labels = labels_dense.shape[0]
-  index_offset = numpy.arange(num_labels) * num_classes
-  labels_one_hot = numpy.zeros((num_labels, num_classes))
-  labels_one_hot.flat[index_offset + labels_dense.ravel()] = 1
-  return labels_one_hot
-
-
-class DataSet(object):
-
-  def __init__(self,
-               images,
-               labels,
-               fake_data=False,
-               one_hot=False,
-               dtype=dtypes.float32,
-               reshape=True,
-               seed=None):
-    """Construct a DataSet.
-    one_hot arg is used only if fake_data is true.  `dtype` can be either
-    `uint8` to leave the input as `[0, 255]`, or `float32` to rescale into
-    `[0, 1]`.  Seed arg provides for convenient deterministic testing.
-    """
-    seed1, seed2 = random_seed.get_seed(seed)
-    # If op level seed is not set, use whatever graph level seed is returned
-    numpy.random.seed(seed1 if seed is None else seed2)
-    dtype = dtypes.as_dtype(dtype).base_dtype
-    if dtype not in (dtypes.uint8, dtypes.float32):
-      raise TypeError('Invalid image dtype %r, expected uint8 or float32' %
-                      dtype)
-    if fake_data:
-      self._num_examples = 10000
-      self.one_hot = one_hot
-    else:
-      assert images.shape[0] == labels.shape[0], (
-          'images.shape: %s labels.shape: %s' % (images.shape, labels.shape))
-      self._num_examples = images.shape[0]
-
-      # Convert shape from [num examples, rows, columns, depth]
-      # to [num examples, rows*columns] (assuming depth == 1)
-      if reshape:
-        assert images.shape[3] == 1
-        images = images.reshape(images.shape[0],
-                                images.shape[1] * images.shape[2])
-      if dtype == dtypes.float32:
-        # Convert from [0, 255] -> [0.0, 1.0].
-        images = images.astype(numpy.float32)
-        images = numpy.multiply(images, 1.0 / 255.0)
-    self._images = images
-    self._labels = labels
-    self._epochs_completed = 0
-    self._index_in_epoch = 0
-
-  @property
-  def images(self):
-    return self._images
-
-  @property
-  def labels(self):
-    return self._labels
-
-  @property
-  def num_examples(self):
-    return self._num_examples
-
-  @property
-  def epochs_completed(self):
-    return self._epochs_completed
-
-  def next_batch(self, batch_size, shuffle=True):
-    """Return the next `batch_size` examples from this data set."""
-    
-    start = self._index_in_epoch
-    # Shuffle for the first epoch
-    if self._epochs_completed == 0 and start == 0 and shuffle:
-      perm0 = numpy.arange(self._num_examples)
-      numpy.random.shuffle(perm0)
-      self._images = self.images[perm0]
-      self._labels = self.labels[perm0]
-    # Go to the next epoch
-    if start + batch_size > self._num_examples:
-      # Finished epoch
-      self._epochs_completed += 1
-      # Get the rest examples in this epoch
-      rest_num_examples = self._num_examples - start
-      images_rest_part = self._images[start:self._num_examples]
-      labels_rest_part = self._labels[start:self._num_examples]
-      # Shuffle the data
-      if shuffle:
-        perm = numpy.arange(self._num_examples)
-        numpy.random.shuffle(perm)
-        self._images = self.images[perm]
-        self._labels = self.labels[perm]
-      # Start next epoch
-      start = 0
-      self._index_in_epoch = batch_size - rest_num_examples
-      end = self._index_in_epoch
-      images_new_part = self._images[start:end]
-      labels_new_part = self._labels[start:end]
-      return numpy.concatenate((images_rest_part, images_new_part), axis=0) , numpy.concatenate((labels_rest_part, labels_new_part), axis=0)
-    else:
-      self._index_in_epoch += batch_size
-      end = self._index_in_epoch
-      return self._images[start:end], self._labels[start:end]
-
-
-def read_data_sets(train_dir,
-                   one_hot=False,
-                   dtype=dtypes.float32,
-                   reshape=True,
-                   validation_size=5000,
-                   seed=None):
-
-  validation_images = train_images[:validation_size]
-  validation_labels = train_labels[:validation_size]
-  train_images = train_images[validation_size:]
-  train_labels = train_labels[validation_size:]
-
-
-  options = dict(dtype=dtype, reshape=reshape, seed=seed)
-
-  train = DataSet(train_images, train_labels, **options)
-  validation = DataSet(validation_images, validation_labels, **options)
-  test = DataSet(test_images, test_labels, **options)
-
-  return base.Datasets(train=train, validation=validation, test=test)
-
-
-def load_mnist(train_dir='MNIST-data'):
-  return read_data_sets(train_dir)
+    # Wait for threads to stop
+    coord.join(threads)
+    sess.close()
